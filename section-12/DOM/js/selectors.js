c8f4bfb65ed9c4dac165594bedad1d5833b54f7e
elidvenega <venegaelid@gmail.com>
2022-02-13 18:16:20 -0800

    Work with DOM and created a shopping list

---
+++ section-12/DOM/js/selectors.js
@@ -0,0 +1,31 @@
+// getting id and elements
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
+
+// functions where I create li elements and append them to list
+function inputLength() {
+  return input.value.length;
+}
+
+function createListElement() {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterKeypress(e) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+// events
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
---
+++ section-12/exercise-shopping-list/script.js
@@ -0,0 +1,37 @@
+/* 
+   1.Add button to li tag
+   2.Make sure it can delete items
+*/
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+  return input.value.length;
+}
+
+function createListElement() {
+  var li = document.createElement("li");
+  var btn = document.createElement("button");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  ul.appendChild(btn);
+  input.value = "";
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
