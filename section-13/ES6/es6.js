d658572eecce9864ed67a91ebee649d6695d439d
elidvenega <venegaelid@gmail.com>
2022-02-17 07:01:08 -0800

    I finished the ES6 section

--- section-13/ES6/es6.js
+++ section-13/ES6/es6.js
@@ -1,1 +1,30 @@
-// let and const
+// Destructuring
+const obj = {
+  player: "Bobby",
+  experience: 100,
+  wizardLevel: false,
+};
+
+const { player, experience, wizardLevel } = obj;
+
+const name = "john snow";
+
+const obj2 = {
+  [name]: "hello",
+  [1 + 2]: "hihi",
+};
+
+const a = "Mike";
+const b = true;
+const c = 1;
+
+const obj3 = {
+  a,
+  b,
+  c,
+};
+
+// Symbol
+let sym1 = Symbol();
+let sym2 = Symbol("foo");
+let sym3 = Symbol("foo");
