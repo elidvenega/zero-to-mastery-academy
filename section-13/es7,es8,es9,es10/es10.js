6b4d031727856762d5b4131679d3faa4ce7eee0c
elid venega <venegaelid@gmail.com>
2022-02-26 09:34:07 -0800

    Work on ES10

---
+++ section-13/es7,es8,es9,es10/es10.js
@@ -0,0 +1,85 @@
+const array = [1,2,3,[4,5,6]];
+// you can change how many layers you want to flat an array
+array.flat() 
+
+const obj = [{
+    ex:1,
+    num:1
+},
+{
+    ex:2,
+    num:2
+},
+{
+    ex:3,
+    num:3
+}]
+
+console.log(obj);
+
+const user1 = '        eddy@gmail.com';
+const user2 = 'alex@gmail.com    ';
+console.log(user1.trimStart());
+console.log(user2.trimEnd());
+
+// fromEntries
+
+try {
+
+} catch {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
