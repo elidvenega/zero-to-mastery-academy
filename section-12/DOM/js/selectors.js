5a26cd438da623a92864fb2561e1297f63c9a567
elidvenega <venegaelid@gmail.com>
2022-03-24 23:25:57 -0700

    Got project working

--- section-12/DOM/js/selectors.js
+++ section-12/DOM/js/selectors.js
@@ -1,4 +1,15 @@
+'use strict';
+// 1.Make sure you 100% understand the problem. ask the right questions
+// to get a clear picture of the problem.
+// 2.Divide and conquer: Break a big problem into smaller sub-problems
+// 3.Don't be afraid to do as much research as you have to
+// 4.For bigger problems, write pseudo-code
+// before writing the actual code.
+
 // 1.ADD Button to delete Items on list
+// 2.Make sure btn is inserted into code
+// 3.Google and stack overflow
+
 
 
 // getting id and elements
@@ -12,11 +23,18 @@ function inputLength() {
 }
 
 function createListElement() {
+  const div = document.createElement('div');
   const li = document.createElement("li");
   const btn = document.createElement("button");
+  div.classList.add('wrapper');
+  ul.appendChild(div)
+  div.append(li,btn)
   li.appendChild(document.createTextNode(input.value));
   li.appendChild(btn);
   ul.appendChild(li);
+  btn.innerHTML = "delete"
+  btn.style.margin = '5px';
+  btn.classList.add("delClass");
   input.value = "";
 }
 
@@ -33,9 +51,24 @@ function addListAfterKeypress(e) {
   }
 }
 
+// functions that were created to delete button and things on list
+function doneTask(task) {
+  if (task.target.tagName === "LI") {
+    task.target.classList.toggle("done");
+  }
+}
+
+function deleteListElement(element) {
+  if (element.target.className === "delClass") {
+    element.target.parentElement.remove();
+  }
+}
+
+function handleUlClick(element) {
+  doneTask(element);
+  deleteListElement(element);
+}
 // events
+ul.addEventListener("click", handleUlClick);
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
-
-
-
