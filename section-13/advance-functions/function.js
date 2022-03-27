7ec0ed745606fee4cec07b71e4ece18695a89845
elidvenega <venegaelid@gmail.com>
2022-03-26 22:59:56 -0700

    Worked on advanced functions

--- section-13/advance-functions/function.js
+++ section-13/advance-functions/function.js
@@ -1,4 +1,6 @@
-// closures
+// closures - a function ran, the function executes. Its never going to
+// execute again but its going to remember that there are references to those variables
+// so the child scope always has access to the parent scope
 const first = () => {
   const greet = "Hi";
   const second = () => {
@@ -17,9 +19,20 @@ newFunc();
 // Currying
 const multiply = (a, b) => a * b;
 const curriedMultiply = (a) => (b) => a * b;
-curriedMultiply(3)(4);
+console.log(curriedMultiply(3)(4));
 
 // Compose
 const compose = (f, g) => (a) => f(g(a));
-
+const sum = (num) => num + 1;
+console.log(compose(sum,sum)(5));
 // Avoiding side effects, functional purity
+
+
+
+
+
+
+
+
+
+
