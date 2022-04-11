ca1fc482772fc6b585bf41637db693d98f34dc68
elidvenega <venegaelid@gmail.com>
2022-04-11 06:54:38 -0700

    Started working on last challenge problem

---
+++ section-24/santas_problem/problem1.js
@@ -0,0 +1,14 @@
+// going to use fs module
+// framework
+/* 
+1.Understand
+2.Plan
+3.Divide
+4.Stuck start over again
+
+*/
+// santa's snow machine does not work need to get 25 stars
+// too collect two stars need to solve 2 puzzles
+// Understand how fs module works
+// Read documentation
+const fs = require('fs');
