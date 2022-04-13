368b9645877fdaba45aeede427d166157996bb3b
elidvenega <venegaelid@gmail.com>
2022-04-13 07:20:58 -0700

    Worked on node problem

--- section-24/santas_problem/problem1.js
+++ section-24/santas_problem/problem1.js
@@ -2,13 +2,29 @@
 // framework
 /* 
 1.Understand
+// Santa's snow machine does not work so need to solve 1st puzzle
+// Understand the puzzle been given to me
 2.Plan
+// Read on fs module
+// Use a function to solve problem
+// Watch Udemy video agin for instructions
 3.Divide
+// Break code into smaller parts
 4.Stuck start over again
-
 */
-// santa's snow machine does not work need to get 25 stars
-// too collect two stars need to solve 2 puzzles
+
+/* Solution
+final Santa floor = (all the time Santa goes up) -
+ (all the time Santa goes down)
+*/
+
+// santa's snow machine does not work need to solve 1'st puzzle
+// too collect stars need to solve 2 puzzles
 // Understand how fs module works
 // Read documentation
-const fs = require('fs');
+
+const fs = require("fs");
+
+fs.readFile("./challenge.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
