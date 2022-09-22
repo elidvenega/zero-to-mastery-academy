3489a7f192d2b4c8fccb7eb342ab8ca5616eb36e
elidvenega <venegaelid@gmail.com>
2022-09-22 10:01:12 -0700

    Worked on scope

--- section-13/scope/main.js
+++ section-13/scope/main.js
@@ -1,49 +1,46 @@
 "use strict";
-// scope is variable access
-var b = "can i access";
 
-function bb() {
-  var a = "hello";
+const output_scope = document.querySelector(".scope_output");
+
+const functionScope = () => {
+  let sentenceOne = "This lesson was on";
+  let sentenceTwo = "Scope";
+
+  return `${sentenceOne} ${sentenceTwo}`;
+};
+
+//output_scope.innerHTML = functionScope();
+output_scope.textContent = functionScope();
+// scope is letiable access
+let b = "can i access";
+
+const bb = () => {
+  let a = "hello";
   console.log(b);
-}
+};
 
 // Root scope (window)
-var fun = 5;
-function funFunction() {
+let fun = 5;
+const funFunction = () => {
   // child scope
-  var fun = "helloooo";
-  console.log(1,fun);
-}
+  let fun = "helloooo";
+  console.log(1, fun);
+};
 
-function funerFunction() {
+const funerFunction = () => {
   // child scope
-  var fun = "Byeee";
-  console.log(2,fun);
-}
+  let fun = "Byeee";
+  console.log(2, fun);
+};
 
-function funestFunction() {
+const funestFunction = () => {
   // child scope
   fun = "AHHHHHH";
-  console.log(3,fun);
-}
+  console.log(3, fun);
+};
 
-console.log('window',fun);
+console.log("window", fun);
 funFunction();
 funerFunction();
 funestFunction();
 console.log(fun);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
